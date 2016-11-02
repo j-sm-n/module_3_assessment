00404f929f18754829c92a824cac730d5885957b
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-02 11:52:19 -0600

    Finish item crud

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    # require 'pry'; binding.pry
     @item = Item.find_by(params[:id])
     render json: @item
   end
@@ -13,5 +12,22 @@ class Api::V1::ItemsController < ApplicationController
     @item = Item.find_by(params[:id])
     @item.destroy
     render json: { "message": "Item #{params[:id]} was deleted", "status": "204" }
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # require 'pry'; binding.pry
+
+    if @item.save
+      render json: { "message": "Item '#{@item.name}' was created!", "status": "201" }
+    else
+
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :image_url)
   end
 end
