6a9bd24be7a02124163e35f02d4e3d8531adacdd
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-11-02 10:43:56 -0600

    Add best buy service complete

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
   def index
-    raw_stores = BestBuyService.new
-    @total_store_count = raw_stores[:total]
-    # require 'pry'; binding.pry
-    @stores = raw_stores[:stores].map do |store|
+    raw_stores = BestBuyService.new(params[:search])
+    @total_store_count = raw_stores.total_stores
+    
+    @stores = raw_stores.stores.map do |store|
       BestBuyStore.new(store)
     end
   end
--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,25 +1,26 @@
 class BestBuyService
-  # def index
-  #   res = conn.get do |req|
-  #     req.url "v1/stores(area(#{params[:search]},25))"
-  #     req.params['format'] = "json"
-  #     req.params['show'] = "storeId,storeType,longName,distance,city,phone"
-  #     req.params['pageSize'] = 15
-  #     req.params['apiKey'] = ENV['BB_API_KEY']
-  #   end
-  #
-  #   raw_stores = JSON.parse(res.body, symbolize_names: true)
-  #
-  #   @total_store_count = raw_stores[:total]
-  #   # require 'pry'; binding.pry
-  #   @stores = raw_stores[:stores].map do |store|
-  #     BestBuyStore.new(store)
-  #   end
-  # end
-  #
-  # def conn
-  #   Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
-  #     faraday.adapter  Faraday.default_adapter
-  #   end
-  # end
+  attr_reader :stores,
+              :total_stores
+
+  def initialize(zip_code)
+    response = JSON.parse(get_stores(zip_code).body, symbolize_names: true)
+    @total_stores = response[:total]
+    @stores = response[:stores]
+  end
+
+  def get_stores(zip_code)
+    conn.get do |req|
+      req.url "v1/stores(area(#{zip_code},25))"
+      req.params['format'] = "json"
+      req.params['show'] = "storeId,storeType,longName,distance,city,phone"
+      req.params['pageSize'] = 15
+      req.params['apiKey'] = ENV['BB_API_KEY']
+    end
+  end
+
+  def conn
+    Faraday.new(:url => 'https://api.bestbuy.com/') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
 end
